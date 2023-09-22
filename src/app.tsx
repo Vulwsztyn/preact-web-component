478a7cd0af35f6ce1c30d85d02860cf45c3e1045
Artur Mostowski <artur.mostowski@protonmail.com>
2023-09-22 13:12:48 +0200

    eui works

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,16 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
-
+import { EuiButton, EuiFlexGroup, EuiFlexItem, EuiSpacer } from '@elastic/eui'
 export function App() {
   const [count, setCount] = useState(0)
-
+  const value = 'success'
   return (
     <div id='app'>
-      <img src={preactLogo} alt='logo' />
       <div class='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <EuiButton color='success' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </EuiButton>
       </div>
     </div>
   )
