ab6f238ea7fb0af2c1c0da7df3d36aae8bb3b3be
Artur Mostowski <artur.mostowski@protonmail.com>
2023-09-22 10:57:20 +0200

    pnpm

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,16 @@
-import { useState } from "preact/hooks";
-import preactLogo from "./assets/preact.svg";
-import "./app.css";
+import { useState } from 'preact/hooks'
+import preactLogo from './assets/preact.svg'
+import './app.css'
 
 export function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
-    <div id="app">
-      <img src={preactLogo} alt="logo" />
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div id='app'>
+      <img src={preactLogo} alt='logo' />
+      <div class='card'>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
       </div>
     </div>
-  );
+  )
 }
