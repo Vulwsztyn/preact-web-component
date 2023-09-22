e8383a44edd3d2d0a72881b5594927175d85df28
Artur Mostowski <artur.mostowski@protonmail.com>
2023-09-22 12:06:41 +0200

    mui test config

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'preact/hooks'
 import preactLogo from './assets/preact.svg'
 import './app.css'
+import Button from '@mui/material/Button'
 
 export function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,9 @@ export function App() {
     <div id='app'>
       <img src={preactLogo} alt='logo' />
       <div class='card'>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button variant='contained' color='success' onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </Button>
       </div>
     </div>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,27 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import preact from '@preact/preset-vite'
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    lib: {
-      entry: './src/main.tsx',
-      name: 'MyLibrary',
-      formats: ['iife'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  // console.log(JSON.stringify(env, null, 2))
+  return {
+    resolve: {
+      alias: {
+        react: 'preact-compat',
+        'react-dom': 'preact-compat',
+      },
     },
-  },
-  plugins: [preact()],
+    build: {
+      lib: {
+        entry: './src/main.tsx',
+        name: 'MyLibrary',
+        formats: ['iife'],
+      },
+    },
+    plugins: [preact()],
+    define: {
+      'process.env': env,
+      'process.env.NODE_ENV': JSON.stringify(mode),
+    },
+  }
 })
